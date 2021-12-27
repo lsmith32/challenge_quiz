75e7093869015f0952228e9019b8fcb502dfe943
lsmith32 <lmksmith97@gmail.com>
2021-12-26 21:17:57 -0600

    highscore html and JS added and functional

--- assets/game.js
+++ assets/game.js
@@ -24,7 +24,6 @@ var questions = [
         choices: ["22", "9", "13", "16"],
         answer: "16"
     },
-
 ];
 // Declared variables
 var score = 0;
@@ -49,7 +48,6 @@ timer.addEventListener("click", function () {
         holdInterval = setInterval(function () {
             secondsLeft--;
             currentTime.textContent = "Time left: " + secondsLeft;
-
             if (secondsLeft <= 0) {
                 clearInterval(holdInterval);
                 allDone();
@@ -131,4 +129,46 @@ function allDone() {
         createP.textContent = "Your final score is: " + timeRemaining;
         questionsDiv.appendChild(createP2);
     }
+
+ // Label
+ var createLabel = document.createElement("label");
+ createLabel.setAttribute("id", "createLabel");
+ createLabel.textContent = "Enter your initials: ";
+ questionsDiv.appendChild(createLabel);
+ //Input
+ var createInput = document.createElement("input");
+ createInput.setAttribute("type", "text");
+ createInput.setAttribute("id", "initials");
+ createInput.textContent = "";
+ questionsDiv.appendChild(createInput);
+ //submit
+ var createSubmit = document.createElement("button");
+ createSubmit.setAttribute("type", "submit");
+ createSubmit.setAttribute("id", "Submit");
+ createSubmit.textContent = "Submit";
+ questionsDiv.appendChild(createSubmit);
+ //saves initials and score to local storage
+ createSubmit.addEventListener("click", function () {
+     var initials = createInput.value;
+     if (initials === null) {
+         console.log("Please enter initials");
+     } else {
+         var finalScore = {
+             initials: initials,
+             score: timeRemaining
+         }
+         console.log(finalScore);
+         var allScores = localStorage.getItem("allScores");
+         if (allScores === null) {
+             allScores = [];
+         } else {
+             allScores = JSON.parse(allScores);
+         }
+         allScores.push(finalScore);
+         var newScore = JSON.stringify(allScores);
+         localStorage.setItem("allScores", newScore);
+         // Travels to final page
+         window.location.replace("./assets/highscore.html");
+     }
+ });
 }
---
+++ assets/highscore.js
@@ -0,0 +1,20 @@
+//Highscore HTML page
+var highScore = document.querySelector("#highScore");
+var clear = document.querySelector("#clear");
+var goBack = document.querySelector("#goBack");
+
+
+// Retreives local stroage 
+var allScores = localStorage.getItem("allScores");
+allScores = JSON.parse(allScores);
+if (allScores !== null) {
+    for (var i = 0; i < allScores.length; i++) {
+        var createLi = document.createElement("li");
+        createLi.textContent = allScores[i].initials + " " + allScores[i].score;
+        highScore.appendChild(createLi);
+    }
+}
+//takes back to main page
+goBack.addEventListener("click", function () {
+    window.location.replace("./index.html");
+});
